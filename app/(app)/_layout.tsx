4ee73445fa19151be65940c5b1f856267db8dae6
10santiago12 <surrego1303@gmail.com>
2025-05-25 20:17:04 -0500

    no c que cambios son

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -6,6 +6,7 @@ return (
 <Stack>
     <Stack.Screen name="index" options={{title:"Home", headerShown:false}}/>
     <Stack.Screen name="mainpage" options={{title:"Cashier", headerShown:false}}/>
+    <Stack.Screen name="scan" options={{title:"Scan", headerShown:false}}/>
 </Stack>
 );
 }
